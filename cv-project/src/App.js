866ebf533d5dc2abb1ef3b71a092dda5f4913665
nexus0208 <christiandelacruz0208@gmail.com>
2022-02-12 22:27:01 +0800

    code split header component

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,12 +1,66 @@
-import React from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header';
+import Tab from './Tab';
 
-function App() {
-  return (
-    <div>
-      <p className='title'>CV CREATOR</p>
-    </div>
-  )
+class App extends Component {
+      constructor(props) {
+        super(props);
+        this.state = { tabitems: [], text: "" };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);   
+    }
+
+    handleChange(e) {
+        this.setState({ text: e.target.value });
+      }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.state.text.length === 0) {
+          return;
+        }
+        const newItem = {
+          text: this.state.text,
+        };
+        this.setState(state => ({
+          tabitems: state.tabitems.concat(newItem),
+          text: ''
+        }));
+    } 
+
+    render() {
+      return (
+        <div>
+          <Header />
+          <div className='main-container'>
+          <div className='container-one'>
+          <button>Basic Information</button>
+          <button>Experience</button>
+          <button>Education</button>
+          
+          <Tab tabitems={this.state.tabitems} />
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor='new-task'>
+                Add new section
+              </label>
+              <input
+                id="new-task"
+                onChange={this.handleChange}
+                value={this.state.text}
+              />
+              <button>
+                Add
+              </button>
+            </form>
+          </div>
+            <div className='container-two'>
+
+            </div>
+          </div>
+        </div>
+      )
+    }
 }
 
 export default App
---
+++ cv-project/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css';
+
+class Header extends React.Component {    
+    render() {
+        return (
+            <div className='title'>CV CREATOR</div>
+        )
+    }
+};
+
+export default Header
