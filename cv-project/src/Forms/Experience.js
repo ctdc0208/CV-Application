66c51378960b779ce731e77e705dbe7d5185b0d0
nexus0208 <christiandelacruz0208@gmail.com>
2022-02-14 23:03:12 +0800

    add bullet points template

--- cv-project/src/Forms/Experience.js
+++ cv-project/src/Forms/Experience.js
@@ -11,6 +11,14 @@ class Experience extends React.Component {
                     <input type='text' id='title' name='title' autocomplete="off" placeholder='Company Name' size="40" required></input>
                     <input type='text' id='title' name='title' autocomplete="off" placeholder='Start Date' size="40" required></input>
                     <input type='text' id='title' name='title' autocomplete="off" placeholder='End Date' size="40" required></input>
+                    <div className="bullet-points">
+                        <div>Bullet points</div>
+                        <input type='text' id='title' name='title' autocomplete="off" placeholder='Job Title' size="40" required></input>
+                        <input type='text' id='title' name='title' autocomplete="off" placeholder='Company Name' size="40" required></input>
+                        <input type='text' id='title' name='title' autocomplete="off" placeholder='Start Date' size="40" required></input>
+                        <input type='text' id='title' name='title' autocomplete="off" placeholder='End Date' size="40" required></input>
+                        <button>add</button>
+                    </div>
                     <button>Add</button>
                 </form>
 
