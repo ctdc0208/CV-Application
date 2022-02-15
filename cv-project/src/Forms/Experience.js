08eddc27a4cede74d71b0e83e70a780655366ead
nexus0208 <christiandelacruz0208@gmail.com>
2022-02-15 22:33:42 +0800

    Edit text form, experience form

--- cv-project/src/Forms/Experience.js
+++ cv-project/src/Forms/Experience.js
@@ -12,11 +12,10 @@ class Experience extends React.Component {
                     <input type='text' id='title' name='title' autocomplete="off" placeholder='Start Date' size="40" required></input>
                     <input type='text' id='title' name='title' autocomplete="off" placeholder='End Date' size="40" required></input>
                     <div className="bullet-points">
-                        <div>Bullet points</div>
-                        <input type='text' id='title' name='title' autocomplete="off" placeholder='Job Title' size="40" required></input>
-                        <input type='text' id='title' name='title' autocomplete="off" placeholder='Company Name' size="40" required></input>
-                        <input type='text' id='title' name='title' autocomplete="off" placeholder='Start Date' size="40" required></input>
-                        <input type='text' id='title' name='title' autocomplete="off" placeholder='End Date' size="40" required></input>
+                        <div>Skills Acquired</div>
+                        <input type='text' id='title' name='title' autocomplete="off" placeholder='One Skill' size="40" required></input>
+                        <input type='text' id='title' name='title' autocomplete="off" placeholder='Two Skill' size="40" required></input>
+                        <input type='text' id='title' name='title' autocomplete="off" placeholder='Three Skill' size="40" required></input>
                         <button>add</button>
                     </div>
                     <button>Add</button>
