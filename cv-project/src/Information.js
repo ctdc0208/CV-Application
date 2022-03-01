55bebee1dbe7e1e7fc10f47b18c1470922bcae3e
nexus0208 <christiandelacruz0208@gmail.com>
2022-03-01 09:34:31 +0800

    test json parse data

--- cv-project/src/Information.js
+++ cv-project/src/Information.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import './Information.css';
 import Personal from './Forms/Personal';
 import WorkExperience from './Forms/WorkExperience';
@@ -13,9 +14,26 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 const onSubmit = async values => {
     await sleep(300)
     window.alert(JSON.stringify(values, 0, 2))
+    const data = JSON.stringify(values, 0, 2);
+    const parsedata = JSON.parse(data)
+    console.log(parsedata.personal[0].name)
+}
+
+class CVPreview extends React.Component {
+  render() {
+      return (
+          <StyledPage>
+              <StyledSubPage>page</StyledSubPage>
+              <div></div>
+          </StyledPage>
+      )
   }
+}
 
 class Information extends React.Component {   
+
+  
+
     render() {
         return (
             <div className="main-container">
@@ -249,9 +267,14 @@ class Information extends React.Component {
                             Reset
                             </button>
                         </div>
-                        
-                        <pre>{JSON.stringify(values, 0, 2)}</pre>
 
+
+                        
+                        <pre>{
+                         JSON.stringify(values, 0, 2)
+                        }</pre>
+                      
+                        <CVPreview />
                         
                         </form>
                     )}
@@ -260,9 +283,50 @@ class Information extends React.Component {
             </div>
         );
     }
-    
 }
 
+const StyledPage = styled.div`
+    width: 210mm;
+    min-height: 297mm;
+    padding: 20mm;
+    padding-bottom: 0;
+    margin: 10mm auto;
+    border: 1px #D3D3D3 solid;
+    border-radius: 5px;
+    background: white;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+
+    @page {
+        size: A4;
+        margin: 0;
+    }
+
+    @media print {
+        html,
+        body {
+            width: 216mm;
+            height: 279mm;
+        }
+        .page {
+            margin: 0;
+            border: initial;
+            border-radius: initial;
+            width: initial;
+            min-height: initial;
+            box-shadow: initial;
+            background: initial;
+            page-break-after: always;
+        }
+    }
+`;
+
+const StyledSubPage = styled.div`
+    padding: 1cm;
+    border: 5px red solid;
+    height: 256mm;
+    /* .page height -40mm (20 off top/bottom) */
+    outline: 2cm #FFEAEA solid;
+`;
 
 
 export default Information
