3c8ecbdf38752515fe7c8730ee0961c0418c49e9
nexus0208 <christiandelacruz0208@gmail.com>
2023-01-04 09:49:15 +0800

    remove cv preview on information component

--- cv-project/src/Information.js
+++ cv-project/src/Information.js
@@ -19,21 +19,7 @@ const onSubmit = async values => {
     console.log(parsedata.personal[0].name)
 }
 
-class CVPreview extends React.Component {
-  render() {
-      return (
-          <StyledPage>
-              <StyledSubPage>page</StyledSubPage>
-              <div></div>
-          </StyledPage>
-      )
-  }
-}
-
 class Information extends React.Component {   
-
-  
-
     render() {
         return (
             <div className="main-container">
@@ -273,9 +259,7 @@ class Information extends React.Component {
                         <pre>{
                          JSON.stringify(values, 0, 2)
                         }</pre>
-                      
-                        <CVPreview />
-                        
+          
                         </form>
                     )}
                 />
@@ -284,49 +268,6 @@ class Information extends React.Component {
         );
     }
 }
-
-const StyledPage = styled.div`
-    width: 210mm;
-    min-height: 297mm;
-    padding: 20mm;
-    padding-bottom: 0;
-    margin: 10mm auto;
-    border: 1px #D3D3D3 solid;
-    border-radius: 5px;
-    background: white;
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-
-    @page {
-        size: A4;
-        margin: 0;
-    }
-
-    @media print {
-        html,
-        body {
-            width: 216mm;
-            height: 279mm;
-        }
-        .page {
-            margin: 0;
-            border: initial;
-            border-radius: initial;
-            width: initial;
-            min-height: initial;
-            box-shadow: initial;
-            background: initial;
-            page-break-after: always;
-        }
-    }
-`;
-
-const StyledSubPage = styled.div`
-    padding: 1cm;
-    border: 5px red solid;
-    height: 256mm;
-    /* .page height -40mm (20 off top/bottom) */
-    outline: 2cm #FFEAEA solid;
-`;
 
 
 export default Information
