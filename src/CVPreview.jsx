e15851f5def2729867e5540c0c0fb9f3771d1b1e
nexus0208 <christiandelacruz0208@gmail.com>
2023-10-10 17:23:16 +0800

    import redux store

--- src/CVPreview.jsx
+++ src/CVPreview.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import './styles/CVpreview.css';
-// import store from "./Redux/store";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+import FormStateFromRedux from "./Redux/FormStateFromRedux";
 
-// TODO: redux store
+
+export class GetDataForm extends React.Component {
+    render() {
+        return (
+            <FormStateFromRedux form="example" />
+        )
+    }
+}
+
 class CVPreview extends React.Component {
     render() {
         return (
+            <Provider store={store}>
             <div className="preview-container">
-                <div className="preview-container-child">page
+                <div className="preview-container-child">
+                <div form="example"></div>
+                <FormStateFromRedux form="example"/>
                 </div>    
             </div>
+            </Provider>
         )
     }
 }
